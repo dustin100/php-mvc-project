2c90e637f1e313ef9a336f2e00728280cc5d9f0e
Dustin <dmkelly100@gmail.com>
2020-11-25 11:43:23 -0500

    setup database

--- app/config/config.php
+++ app/config/config.php
@@ -1,5 +1,14 @@
 <?php
 
+// DB Parms
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '123456');
+define('DB_NAME', 'dkmvc');
+
+
+
 // App Root
 define('APPROOT', dirname(dirname(__FILE__)));
 
