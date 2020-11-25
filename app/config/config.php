23580bba240f11ad06eae9a9cc39476771c3b1dc
Dustin <dmkelly100@gmail.com>
2020-11-24 20:04:23 -0500

    setup config file

---
+++ app/config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+
+// URL Root
+define('URLROOT', 'http://localhost:8080/php-mvc');
+
+// Site Name
+define('SITENAME', 'Learn PHP');
