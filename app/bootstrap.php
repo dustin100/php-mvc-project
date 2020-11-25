aa24c7684661683903857219f8e3a8718bee45c9
Dustin <dmkelly100@gmail.com>
2020-11-25 18:16:13 -0500

    user registration working

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,6 +2,8 @@
 // Load config
 require_once 'config/config.php';
 
+// load helpers
+require_once 'helpers/url_helper.php';
 
 // Autoload Core Libraries
 spl_autoload_register(function ($className) {
