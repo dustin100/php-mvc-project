4d5935a64ab79309e3ff4a17658e80b7257ae286
Dustin <dmkelly100@gmail.com>
2020-11-25 14:15:48 -0500

    re-DB setup

--- app/config/config.php
+++ app/config/config.php
@@ -3,9 +3,9 @@
 // DB Parms
 
 define('DB_HOST', 'localhost');
-define('DB_USER', '_YOUR_USER_');
-define('DB_PASS', '_YOUR_PASS_');
-define('DB_NAME', '_YOUR_DB_NAME');
+define('DB_USER', 'root');
+define('DB_PASS', '123456');
+define('DB_NAME', 'sharepposts');
 
 
 
@@ -14,7 +14,7 @@ define('APPROOT', dirname(dirname(__FILE__)));
 
 
 // URL Root
-define('URLROOT', '_YOUR_URL_');
+define('URLROOT', 'http://localhost:8080/shareposts');
 
 // Site Name
-define('SITENAME', '_YOUR_SITENAME_');
+define('SITENAME', 'Share Posts');
--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -8,7 +8,7 @@ class Pages extends Controller
     public function index()
     {
         $data = [
-            'title' => 'Welcome',
+            'title' => 'Share Posts',
 
         ];
 
